048b57c1bc44c8fa7a9bbbaeb270e2c54f8a5259
abdelrahman <a.aymanhalawa1@email.com>
2026-04-05 02:33:18 +0300

    add likes table with composite primary key

---
+++ app/models/like.py
@@ -0,0 +1,8 @@
+from app.extensions import db
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, primary_key=True)
