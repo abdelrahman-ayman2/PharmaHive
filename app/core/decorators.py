6f28560c30285a014d4160cd991df1e17a172a58
abdelrahman <a.aymanhalawa1@email.com>
2026-04-15 14:12:18 +0300

    Refactor login_required to support AJAX requests

--- app/core/decorators.py
+++ app/core/decorators.py
@@ -1,4 +1,4 @@
-from flask import url_for, session, redirect, g
+from flask import url_for, session, redirect, g, request, jsonify
 from functools import wraps
 from ..models.user import User
 from ..extensions import db
@@ -9,16 +9,23 @@ def login_required(f):
         user_id = session.get("user_id")
 
         if not user_id:
-            return redirect(url_for("auth.login"))
+            return handle_unauthorized()
         
         user = db.session.get(User, user_id)
         
         if user is None:
             session.clear()
-            return redirect(url_for("auth.login"))
+            return handle_unauthorized()
         
         g.user = user
 
         return f(*args, **kwargs)
     
     return decorator_function
+
+def handle_unauthorized():
+    if request.headers.get("X-Requested-With") == "XMLHttpRequest":
+        return jsonify({"error": "unauthorized"}), 401
+
+    # fallback (HTML)
+    return redirect(url_for("auth.login"))
