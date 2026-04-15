ef82b056c83c0e6c8f9474f1f96496b220f819fd
abdelrahman <a.aymanhalawa1@email.com>
2026-04-15 15:58:55 +0300

    Refactor profile route and template to support guest browsing and proper owner handling

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, session
 from ..core.decorators import login_required
 from ..models.user import User
+from ..extensions import db
 
 users_bp = Blueprint('users', __name__, url_prefix='/u')
 
 @users_bp.route('/<username>')
-@login_required
 def profile(username):
     
-    user = User.query.filter_by(username=username).first_or_404()
-    
-    return render_template("users/profile.html", user=user)
+    owner = User.query.filter_by(username=username).first_or_404()
+
+    user_id = session.get("user_id")
+                           
+    if user_id:
+        user = db.session.get(User, user_id)
+    else:
+        user = None
+        
+    return render_template("users/profile.html", user=user, owner=owner)
